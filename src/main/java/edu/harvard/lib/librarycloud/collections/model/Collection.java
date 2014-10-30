1cabd4f34da383fb0552c7c3ec86d70a00e0dcef
Jeffrey Licht <jeff@podconsulting.com>
2014-10-30 11:02:48 -0400

    Add namespaces for XML responses. Add more fields to Collection class

--- src/main/java/edu/harvard/lib/librarycloud/collections/model/Collection.java
+++ src/main/java/edu/harvard/lib/librarycloud/collections/model/Collection.java
@@ -4,14 +4,53 @@ import javax.persistence.*;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/* Implements a subset of the Dublin Core Collections Application Profile (DCCAP)
+
+	Complete list of fields in the DCCAP:
+
+		Type [dc:type]
+		Collection Identifier [dc:identifier]
+		Title [dc:title]
+		Alternative Title [dcterms:alternative]
+		Description [dcterms:abstract]
+		Size [dcterms:extent]
+		Language [dc:language]
+		Item Type [cld:itemType]
+		Item Format [cld:itemFormat]
+		Rights [dc:rights]
+		Access Rights [dcterms:accessRights]
+		Accrual Method [dcterms:accrualMethod]
+		Accrual Periodicity [dcterms:accrualPeriodicity]
+		Accrual Policy [dcterms:accrualPolicy]
+		Custodial History [dcterms:provenance]
+		Audience [dcterms:audience]
+		Subject [dc:subject]
+		Spatial Coverage [dcterms:spatial]
+		Temporal Coverage [dcterms:temporal]
+		Date Collection Accumulated [dcterms:created]
+		Date Items Created [cld:dateItemsCreated]
+		Collector [dc:creator]
+		Owner [marcrel:OWN]
+		Is Located At [cld:isLocatedAt]
+		Is Accessed Via [cld:isAccessedVia]
+		Sub-Collection [dcterms:hasPart]
+		Super-Collection [dcterms:isPartOf]
+		Catalogue or Index [cld:catalogueOrIndex]
+		Associated Collection [cld:associatedCollection]
+		Associated Publication [dcterms:isReferencedBy]
+
+		TOOD: Include all fields
+		T
+*/
+
+
 @Entity
-@XmlRootElement
+@XmlRootElement(namespace = "http://api.lib.harvard.edu/v2/collection/", name="collection")
 public class Collection  {
 
 	public Collection() {}
 
 	@Id @GeneratedValue
-	@XmlElement(name="id")
 	private int id;
 
 	@Column(nullable = false)
@@ -20,11 +59,27 @@ public class Collection  {
 	@Lob
 	private String summary;
 
+	private int extent;
 
+	private String language;
+
+	@Lob
+	private String rights;
+
+	@Lob
+	private String accessRights;
+
+	@XmlElement(namespace="http://purl.org/dc/elements/1.1", name="identifier")
 	public int getId() {
 		return id;
 	}
 
+	@XmlElement(namespace="http://purl.org/dc/elements/1.1", name="type")
+	public String getCollection() {
+		return "collection";
+	}
+
+	@XmlElement(namespace="http://purl.org/dc/elements/1.1/", name="title")
 	public String getTitle() {
 		return title;
 	}
@@ -33,13 +88,45 @@ public class Collection  {
 		this.title = title;
 	}
 
-
+	@XmlElement(namespace="http://purl.org/dc/terms/", name="abstract")
 	public String getSummary() {
 		return summary;
 	}
 
 	public void setSummary(String summary) {
 		this.summary = summary;
+	}
+
+	@XmlElement(namespace="http://purl.org/dc/terms", name="extent")
+	public int getExtent() {
+		return this.extent;
+	}
+
+	@XmlElement(namespace="http://purl.org/dc/elements/1.1", name="language")
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	@XmlElement(namespace="http://purl.org/dc/elements/1.1", name="rights")
+	public String getRights() {
+		return rights;
+	}
+
+	public void setRights(String rights) {
+		this.rights = rights;
+	}
+
+	@XmlElement(namespace="http://purl.org/dc/terms", name="accessRights")
+	public String getAccessRights() {
+		return accessRights;
+	}
+
+	public void setAccessRights(String accessRights) {
+		this.accessRights = accessRights;
 	}
 
 }
