2d404fffd033bb9c735a5a9712a859e86baacbef
Nick Caramello <nick@podconsulting.com>
2017-03-13 10:27:17 -0400

    stablization testing changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
+-- Create roles
 insert into role (id, name) values (1, 'owner');
 insert into role (id, name) values (2, 'editor');
+-- Create test user
+insert into user (id, email, name, token) values (1, 'nick@podconsulting.com', 'nick caramello', '999999999');
