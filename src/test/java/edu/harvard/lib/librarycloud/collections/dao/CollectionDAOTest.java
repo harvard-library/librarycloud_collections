f0e8941323220e77d44ce647f65a218548ccf690
Brian Hoffman <brianjhoffman@gmail.com>
2018-03-14 12:04:21 -0400

    finish https://jira.huit.harvard.edu/browse/LTSCLOUD-538

--- src/test/java/edu/harvard/lib/librarycloud/collections/dao/CollectionDAOTest.java
+++ src/test/java/edu/harvard/lib/librarycloud/collections/dao/CollectionDAOTest.java
@@ -1,20 +1,30 @@
 package edu.harvard.lib.librarycloud.collections.test;
 
 import java.util.*;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.context.annotation.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 // import org.springframework.boot.test.autoconfigure.orm.jpa.*;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.apache.openjpa.persistence.PersistenceProviderImpl;
+import org.flywaydb.core.Flyway;
 
-import javax.persistence.EntityManager;
+import javax.persistence.*;
+import java.lang.reflect.*;
+import javax.sql.DataSource;
 
 import edu.harvard.lib.librarycloud.collections.model.User;
 import edu.harvard.lib.librarycloud.collections.model.Item;
@@ -23,17 +33,150 @@ import edu.harvard.lib.librarycloud.collections.dao.CollectionDAO;
 
 
 @RunWith( SpringJUnit4ClassRunner.class )
-@ContextConfiguration("file:src/main/resources/spring/applicationContext.xml")
-
+@ContextConfiguration( loader=AnnotationConfigContextLoader.class )
+@Transactional
 public class CollectionDAOTest {
+
+    @Configuration
+    @PropertySource("classpath:test.properties")
+    static class ContextConfiguration {
+
+        @Value( "${db_url}" )
+        String dbUrl;
+
+        @Value( "${db_user}" )
+        String dbUser;
+
+        @Value( "${db_password}" )
+        String dbPassword;
+
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
+
+        @Bean
+        @Autowired
+        public PlatformTransactionManager transactionManager(EntityManagerFactory factory) {
+            JpaTransactionManager txManager = new JpaTransactionManager();
+            txManager.setEntityManagerFactory(factory);
+            return txManager;
+        }
+
+        @Bean
+        Flyway flyway() {
+            Flyway flyway = new Flyway();
+            flyway.setLocations("filesystem:src/main/resources/db/migration/");
+            flyway.setDataSource(dataSource());
+            return flyway;
+        }
+
+
+        @Bean
+        public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource());
+            em.setPackagesToScan(new String[] { "edu.harvard.lib.librarycloud.collections.model.*" });
+            em.setPersistenceProvider(new PersistenceProviderImpl());
+            em.setPersistenceUnitName("integration-test");
+
+            return em;
+        }
+
+
+        @Bean
+        public DataSource dataSource(){
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+            dataSource.setUrl(dbUrl);
+            dataSource.setUsername(dbUser);
+            dataSource.setPassword(dbPassword);
+            return dataSource;
+        }
+
+
+        @Bean
+        public CollectionDAO collectionDao() {
+            CollectionDAO collectionDao = new CollectionDAO();
+            return collectionDao;
+        }
+    }
+
+
+    @PersistenceContext
+    private EntityManager em;
+    private static boolean setUpIsDone = false;
 
     @Autowired
     private CollectionDAO collectionDao;
 
+    @Autowired
+    private Flyway fw;
+
+    @Before
+    @Rollback(false)
+    public void setUpMigration() {
+        if(setUpIsDone) {
+            return;
+        }
+;
+        fw.clean();
+        fw.migrate();
+
+        setUpIsDone = true;
+    }
+
+
+    @Before
+    public void setUpTestUser() {
+
+        User u = new User();
+        u.setName("Test User");
+        u.setEmail("foo@bar.com");
+
+        try {
+            Field f = u.getClass().getDeclaredField("token");
+            f.setAccessible(true);
+            f.set(u, "00000");
+        } catch(NoSuchFieldException e) {
+            throw new RuntimeException("No Such Field");
+        } catch(IllegalAccessException e) {
+            throw new RuntimeException("Illegal Access");
+        }
+
+        em.persist(u);
+    }
+
+    @Test
+    public void testCreatingFullCollectionRecords() {
+        User u = collectionDao.getUserForAPIToken("00000");
+        Collection c = new Collection();
+        c.setTitle("title");
+        c.setDcp(true);
+        c.setPublic(true);
+        c.setAbstract("abstract");
+        c.setThumbnailUrn("http://thumb.com");
+        c.setCollectionUrn("http://coll.com");
+        c.setBaseUrl("http://base.com");
+
+
+        Integer cId = collectionDao.createCollection(c, u);
+
+        c = collectionDao.getCollection(cId);
+        assertEquals("title", c.getTitle());
+        assertEquals(true, c.isDcp());
+        assertEquals(true, c.isPublic());
+        assertEquals("abstract", c.getAbstract());
+        assertEquals("http://thumb.com", c.getThumbnailUrn());
+        assertEquals("http://coll.com", c.getCollectionUrn());
+        assertEquals("http://base.com", c.getBaseUrl());
+    }
+
+
     @Test
     public void testDeletingCollectionsWithSharedItems() {
 
-        Boolean result;
+        boolean result;
         String c1Title = Long.toHexString(Double.doubleToLongBits(Math.random()));
         String c2Title = Long.toHexString(Double.doubleToLongBits(Math.random()));
 
@@ -41,7 +184,7 @@ public class CollectionDAOTest {
         String item2Id = Long.toHexString(Double.doubleToLongBits(Math.random()));
 
 
-        User u = collectionDao.getUserForAPIToken("555555555");
+        User u = collectionDao.getUserForAPIToken("00000");
         Item i1 = new Item();
         i1.setItemId(item1Id);
         Item i2 = new Item();
@@ -53,10 +196,6 @@ public class CollectionDAOTest {
 
         Integer c1Id = collectionDao.createCollection(c1, u);
         Integer c2Id = collectionDao.createCollection(c2, u);
-
-
-        System.out.println(c1.getId());
-        System.out.println(c2.getId());
 
         result = collectionDao.addToCollection(c1Id, i1);
         result = collectionDao.addToCollection(c2Id, i1);
@@ -78,8 +217,6 @@ public class CollectionDAOTest {
 
         // Delete collection 2!
         collectionDao.deleteCollection(c2Id);
-
-        //TODO - make sure it's gone
 
         // Make sure collection 1 still has item 1
         c1Items = collectionDao.getItemsByCollection(c1Id);
